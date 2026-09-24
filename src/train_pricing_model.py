3536ff4ecb7dd8d70b04c856a570d1fed0837ea9
Kostas <kostasr500@gmail.com>
2026-09-24 14:02:57 +0300

    update req files and start training model

---
+++ src/train_pricing_model.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+
